5cc5108e0925c16b5ffb76808bfada16fdf1a9af
foranee <lesha.belikov2005@yandex.ru>
2020-10-08 21:29:14 +0300

    Updates

---
+++ precache-manifest.de5df5bda7b90f5a0839d97f32b7fa3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a03dabe2e8b463e03f46ff38ce35cf6",
+    "url": "/weni/index.html"
+  },
+  {
+    "revision": "341b841ca5b5eababa6c",
+    "url": "/weni/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b4dda8a70404bfe150f5",
+    "url": "/weni/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "341b841ca5b5eababa6c",
+    "url": "/weni/static/js/2.166b20b2.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/weni/static/js/2.166b20b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4dda8a70404bfe150f5",
+    "url": "/weni/static/js/main.23f6fb95.chunk.js"
+  },
+  {
+    "revision": "3195fde33e885666fe9b",
+    "url": "/weni/static/js/runtime-main.6d3fe411.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/weni/static/media/persik.4e1ec840.png"
+  }
+]);
