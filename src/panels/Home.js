33585ec4828268314bf4848b94441cb786ff4e9c
foranee <lesha.belikov2005@yandex.ru>
2020-10-10 15:26:44 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -26,7 +26,7 @@ const Home = ({ id, go, fetchedUser}) => (
 				</Button>
 			</Div>
 			<Div>
-				<img className="Battom" src={battom}>
+				<img className="Battom" src={battom}/>
 			</Div>
 		</Group>
 	</Panel>
