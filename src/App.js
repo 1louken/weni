002696474304d99b116ce1510ac2f631af94cacb
foranee <lesha.belikov2005@yandex.ru>
2020-10-10 09:38:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,14 +40,6 @@ const App = () => {
   		database: "forane$fnor",
   		password: "Tosha3301Alex2005"
 	});
- 	connection.connect(function(err){
-    	if (err) {
-      		return console.error("Ошибка: " + err.message);
-    	}
-    	else{
-      		console.log("Подключение к серверу MySQL успешно установлено");
-    	}
- 	});
 
 	function add() {	
 		connection.query("INSERT INTO slovar (inpus, answes) VALUES (0, 0)", function(err, results) {
