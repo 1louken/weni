0572f488d04b50458cc85e9e9953ab5544a7e79b
foranee <lesha.belikov2005@yandex.ru>
2020-10-10 21:48:48 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -6,6 +6,7 @@ import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Body from '@vkontakte/vkui/dist/components/Body/Body';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 import battom from '../img/battom.png';
