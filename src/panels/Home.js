a3c5fe1191e7b306f6fca3e3056d292466fc1a31
foranee <lesha.belikov2005@yandex.ru>
2020-11-05 01:52:52 +0300

    'commot2222'

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -17,16 +17,12 @@ const Home = ({ id, go, fetchedUser}) => (
 	<Panel id={id}>
 		<PanelHeader>Energy Coin</PanelHeader>
 		<Group>
-			<Div className="Menu_block">
-				<Div className="menu">
-					<img className="Menu" src={menu}/>
-					<Div className="Top_botton" onClick={go} data-to="top"></Div>
-					<Div className="Trade_botton" onClick={go} data-to="trade"></Div>
-					<Div className="Business_botton" onClick={go} data-to="business"></Div>
-					<Div className="Shop_botton" onClick={go} data-to="shop"></Div>
-				</Div>
-				<img className="Battom" src={battom}/>
-			</Div>
+			<img className="Menu" src={menu}/>
+			<Div className="Top_botton" onClick={go} data-to="top"></Div>
+			<Div className="Trade_botton" onClick={go} data-to="trade"></Div>
+			<Div className="Business_botton" onClick={go} data-to="business"></Div>
+			<Div className="Shop_botton" onClick={go} data-to="shop"></Div>
+			<img className="Battom" src={battom}/>
 		</Group>
 	</Panel>
 );
