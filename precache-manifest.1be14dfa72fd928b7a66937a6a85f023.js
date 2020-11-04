8d4078280f7aa338d9aaad5aaa503b514bacac48
foranee <lesha.belikov2005@yandex.ru>
2020-11-04 21:12:18 +0300

    Updates

--- precache-manifest.1be14dfa72fd928b7a66937a6a85f023.js
+++ precache-manifest.1be14dfa72fd928b7a66937a6a85f023.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "432f97ffe9a9a3c1acb215158581db87",
+    "revision": "f20c551e6a355ae8f8975741c40664df",
     "url": "/weni/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weni/static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "febdfac24f434b2a6036",
+    "revision": "c91e49a0367d78e7e649",
     "url": "/weni/static/css/main.2d597de4.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weni/static/js/2.c387a5f0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "febdfac24f434b2a6036",
-    "url": "/weni/static/js/main.d88d3862.chunk.js"
+    "revision": "c91e49a0367d78e7e649",
+    "url": "/weni/static/js/main.36b4ea34.chunk.js"
   },
   {
     "revision": "3195fde33e885666fe9b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weni/precache-manifest.775f87d86513809b8af433d3dd48ca68.js"
+  "/weni/precache-manifest.1be14dfa72fd928b7a66937a6a85f023.js"
 );
 
 self.addEventListener('message', (event) => {
