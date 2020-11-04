982a2025e7567ea906581abf74a458f918d745e8
foranee <lesha.belikov2005@yandex.ru>
2020-11-04 23:21:47 +0300

    'Commit1020'

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -16,13 +16,15 @@ import './Style.css';
 const Home = ({ id, go, fetchedUser}) => (
 	<Panel id={id}>
 		<PanelHeader>Energy Coin</PanelHeader>
-		<Group className="body">
+		<Group>
 			<Div className="Menu_block">
-				<img className="Menu" src={menu}/>
-				<Div className="Top_botton" onClick={go} data-to="top"></Div>
-				<Div className="Trade_botton" onClick={go} data-to="trade"></Div>
-				<Div className="Business_botton" onClick={go} data-to="business"></Div>
-				<Div className="Shop_botton" onClick={go} data-to="shop"></Div>
+				<Div className="menu">
+					<img className="Menu" src={menu}/>
+					<Div className="Top_botton" onClick={go} data-to="top"></Div>
+					<Div className="Trade_botton" onClick={go} data-to="trade"></Div>
+					<Div className="Business_botton" onClick={go} data-to="business"></Div>
+					<Div className="Shop_botton" onClick={go} data-to="shop"></Div>
+				</Div>
 				<img className="Battom" src={battom}/>
 			</Div>
 		</Group>
