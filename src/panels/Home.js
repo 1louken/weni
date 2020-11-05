5012b508530ddc1faea32422e82c163aa5827c85
foranee <lesha.belikov2005@yandex.ru>
2020-11-05 23:04:09 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -26,7 +26,7 @@ const Home = ({ id, go, fetchedUser, bat}) => (
 			<Div className="Trade_botton" onClick={go} data-to="trade"></Div>
 			<Div className="Business_botton" onClick={go} data-to="business"></Div>
 			<Div className="Shop_botton" onClick={go} data-to="shop"></Div>
-			<img id="Battom1" className="Battom" src={battom} onClick='changeStyle(id)'/>
+			<img id="Battom1" className="Battom" src={battom} onClick='changeStyle("Battom1")'/>
 		</Group>
 	</Panel>
 );
