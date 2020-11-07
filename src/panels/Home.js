79609499a61b82643e19ebbd15bc01f82ce7907b
foranee <lesha.belikov2005@yandex.ru>
2020-11-07 23:04:53 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -20,7 +20,7 @@ function ChangeStyl(Element) {
 }
 
 function ChangeStyle(Element) {
-	Element.style.background-color: = 'white';
+	Element.style.background-color = 'white';
 }
 
 const Home = ({ id, go, fetchedUser}) => (
